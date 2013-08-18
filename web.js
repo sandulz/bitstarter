d4cddf66a08b0f9cf275b10c5bfff4ce23b66b65
Alexander Close <alexander.close@gmail.com>
2013-08-18 04:28:16 +0000

    Another web.js revision

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 var express = require('express');
-var app = express();
-app.use(express.logger());
+var app = express.createServer(express.logger);
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var fs = require('fs');
+	var buf = fs.readFileSync('index.html', 'utf8');
+	response.send(buf.toString());
 });
 
+//  response.send('Hello World2!'); });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
