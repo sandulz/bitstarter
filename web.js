dc9e0b4875766a41433220a4b5e944d44ce5001e
Alexander Close <alexander.close@gmail.com>
2013-08-19 02:57:19 +0000

    Edited product desc in index.html

--- web.js
+++ web.js
@@ -3,8 +3,13 @@ var app = express.createServer(express.logger);
 
 app.get('/', function(request, response) {
 	var fs = require('fs');
+<<<<<<< HEAD
 	var buf = fs.readFileSync('index.html', 'utf8');
 	response.send(buf.toString());
+=======
+	var buf = new Buffer(256);
+	response.send(buf.toString('utf8', fs.readFileSync("index.html")));
+>>>>>>> 79dc52c0de4ed8c861f7d41445bd22bb18f2c0e4
 });
 
 //  response.send('Hello World2!'); });
